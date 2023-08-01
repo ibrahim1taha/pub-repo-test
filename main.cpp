ab45cf30b8aece2d7954daa3391097eefe595580
ibrahim1taha <1ibrahim1mohamed1@gmail.com>
2023-08-01 23:41:22 +0300

    commit in terminal

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main()
 {
     cout << "hello world in first code in my githup account";
-    cout << "here new line in code added in githup editor "; 
+    cout << "here new line in code added in githup editor ";
+    int x;
+    cin >> x;
+    cout << x;
 }
