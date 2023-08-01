0c2889b2066c3b12d33fccfaaf67f4005f9e3bfc
ibrahim1taha <1ibrahim1mohamed1@gmail.com>
2023-08-01 23:48:27 +0300

    hh

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int func(int x)
+{
+    return x * 8;
+}
 int main()
 {
     cout << "hello world in first code in my githup account";
     cout << "here new line in code added in githup editor ";
-    int x;
-    cin >> x;
-    cout << x;
-    cout << x * 2;
+    int x = 2;
+
+    cout << func(x);
 }
